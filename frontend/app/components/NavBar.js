f25a12e47f38fdc7f134ea333b5a98d6be27b48f
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-07-21 00:07:22 +0530

    frontend working

--- frontend/app/components/NavBar.js
+++ frontend/app/components/NavBar.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function NavBar(){
     const session = useSession();
     return <div className="px-8 py-8 w-full text-white flex justify-end gap-4 ">
-        <div>{session.data.user?.name.split(" ")[0]}</div>
+        {/*<div>{session.data.user?.name.split(" ")[0]}</div>*/}
         <div className=" hover:scale-105">
             <Link href="/dashboard">
             Home
