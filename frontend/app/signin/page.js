39e55185fe0a4fd0a660a1c110e1a96a144dbbe8
Mohit Hingorani <Mohithingorani2003@gmail.com>
2024-07-21 00:41:11 +0530

    managed nextauth sessions
    managed nextauth sessions

--- frontend/app/signin/page.js
+++ frontend/app/signin/page.js
@@ -1,19 +1,18 @@
-"use client"
+"use client";
 
 import { signIn } from "next-auth/react";
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Signin() {
-
     return (
-        <div className="flex flex-col justify-center items-center h-screen bg-slate-300" >
-            <div className="grid gap-6 mb-6 border px-4 py-6 rounded-xl shadow-lg bg-white">
+        <div className="flex flex-col justify-center items-center h-screen bg-slate-300 bg-cover bg-center" style={{ backgroundImage: 'url("bg2.webp")', backgroundSize: "cover" }}>
+            <div className="grid gap-6 mb-6 border px-4 py-8 rounded-xl shadow-lg bg-white">
                 <div className="text-center font-medium text-4xl col-span-2 text-black px-3">
                     Register For Free
                 </div>
                 <button
                     onClick={async () => {
-                        await signIn("google", { callbackUrl: 'http://localhost:3000/dashboard' })
+                        await signIn("google", { callbackUrl: 'http://localhost:3000/dashboard' });
                     }}
                     className="hover:shadow-md text-center border border-gray-300 p-2.5 rounded-lg bg-gray-50 flex justify-center items-center gap-3 col-span-2"
                 >
@@ -22,5 +21,6 @@ export default function Signin() {
                 </button>
             </div>
         </div>
-    )
+    );
 }
+
