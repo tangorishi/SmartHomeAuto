56d5df4147bea8ac1a8e4221bf1cafd6d6a921e7
Mohit Hingorani <Mohithingorani2003@gmail.com>
2024-07-21 06:23:12 +0530

    signout bug fix
    made signout asynchronous

--- frontend/app/components/NavBar.js
+++ frontend/app/components/NavBar.js
@@ -4,42 +4,44 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function NavBar() {
-    const session = useSession();
-    return <div className="px-8 py-8 w-full text-white gap-4">
-        <div className="flex justify-between items-center">
-            <div className=" flex justify-start items-center gap-2">
-                <Image src={session.data.user.image} width="40" height="40" alt="profile image" className="rounded-full" />
+    const { data: session, status } = useSession();
+    return (
+        <div className="px-8 py-8 w-full text-white gap-4">
+            <div className="flex justify-between items-center">
+                <div className="flex justify-start items-center gap-2">
+                    {session && (
+                        <>
+                            <Image src={session.user.image} width="40" height="40" alt="profile image" className="rounded-full" />
+                            <div>
+                                {session.user.name}
+                            </div>
+                        </>
+                    )}
+                </div>
                 <div>
-                    {session?.data.user.name}
-                </div>
-            </div>
-            <div>
-                <div className="flex justify-end gap-2  items-center">
-                    {session.status == "unauthenticated" ?
-                        <button className=" hover:scale-105" onClick={() => signIn()}>
-                            Login
-                        </button> : null
-                    }
-                    {session.status === "authenticated" ?
-                        <button className=" hover:scale-105" onClick={() => signOut()}>
-                            Logout
-                        </button> : null
-                    }
-                    <div className=" hover:scale-105">
-                        <Link href="/dashboard">
-                            Home
-                        </Link>
-                    </div>
-
-                    <div className=" hover:scale-110">
-                        <Link href="/usage">
-                            Consumption
-                        </Link>
+                    <div className="flex justify-end gap-2 items-center">
+                        {status === "unauthenticated" ? (
+                            <button className="hover:scale-105" onClick={() => signIn()}>
+                                Login
+                            </button>
+                        ) : (
+                            <button className="hover:scale-105" onClick={async () => await signOut()}>
+                                Logout
+                            </button>
+                        )}
+                        <div className="hover:scale-105">
+                            <Link href="/dashboard">
+                                Home
+                            </Link>
+                        </div>
+                        <div className="hover:scale-110">
+                            <Link href="/usage">
+                                Consumption
+                            </Link>
+                        </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-    </div>
+    );
 }
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,7 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/dashboard");
   return (
     
     <>
